38c9b7b9b11a7fa13ec2a41602cb329a475d388b
Andrew Van Wormer <a.vwormer@gmail.com>
2019-05-08 20:44:39 -0700

    Create api fetch and set state with response

--- client/src/app.js
+++ client/src/app.js
@@ -1,15 +1,62 @@
 import React, { Component } from 'react';
+//import { objectExpression } from '@babel/types';
 
 class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            suh: 'hey'
+            films: [],
+            people: []
         }
     }
 
     componentDidMount() {
-        //make api call
+        this.getSwapiFilms();
+        this.getSwapiPeople();
+    }
+
+    //would it be better to make 
+    getSwapiFilms(){
+        fetch('https://swapi.co/api/films/')
+            .then(res => res.json())
+            .then((result) => {
+                this.setState({
+                    films: result.results
+                })
+            })
+    }
+
+    getSwapiPeople(){
+        //consider using the id at end of url to store as object
+        //check if api returns results sorted by that ID
+        let allPeople = []
+
+        let recursePages = (page = '') => {
+            fetch(`https://swapi.co/api/people/${page}`)
+            .then(res => res.json())
+            .then((result) => {
+    
+                allPeople = allPeople.concat(result.results)
+
+                if (result.next) {
+                    let pageNumber = result.next.slice(-1);
+                    let pageQuery = `?page=${pageNumber}`
+                    recursePages(pageQuery)
+                } else {
+                    return allPeople;
+                }
+            })
+            .then((result) => {
+                this.setState({
+                    people: result
+                })
+                return;
+            })
+            .then(()=>console.log(this.state))
+        }
+
+        recursePages();
+        
     }
 
     render() {
